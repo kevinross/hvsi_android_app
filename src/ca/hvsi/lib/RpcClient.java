48cae0cb59ddae6c3a9dfcd5d17c13f7348dbc8a
Kevin Ross <r0ssar00@gmail.com>
2013-08-05 20:28:31 -0400

    object caching in rpclib

--- src/ca/hvsi/lib/RpcClient.java
+++ src/ca/hvsi/lib/RpcClient.java
@@ -34,6 +34,7 @@ import org.joda.time.format.ISODateTimeFormat;
 public abstract class RpcClient {
 	protected String base_endpoint = null;
 	private String endpoint = null;
+	private HashMap<String, HashMap<ArrayWrapper, Object>> cache = null;
 	public Interface interface_ = null;
 	private CookieStore jar = Http.jar();//(ca.hvsi.app.API.context());
 	private HttpClient client = Http.client();
@@ -45,6 +46,7 @@ public abstract class RpcClient {
 	public RpcClient(String base_endpoint, String endpoint) {
 		this.base_endpoint = base_endpoint;
 		this.endpoint = base_endpoint + ((endpoint != null)?("/" + endpoint):"");
+		cache = new HashMap<String, HashMap<ArrayWrapper, Object>>();
 		this.interface_ = (Interface) __rpccall__(new FunctionCall("__interface__"));
 	}
 	public abstract Object[] __marshall_args__(Object... args);
@@ -110,10 +112,39 @@ public abstract class RpcClient {
 		}
 		return __resolve_references__(valuestr);
 	}
+	public void flush() {
+		cache.clear();
+	}
+	private void set(String endpoint, FunctionCall func, Object result) {
+		HashMap<ArrayWrapper, Object> inner = new HashMap<ArrayWrapper, Object>();
+		inner.put(new ArrayWrapper(func.args), result);
+		cache.put(endpoint + '/' + func.func, inner);
+	}
+	private boolean has(String endpoint, FunctionCall func) {
+		System.out.println(func.func + " in cache? " + ((Boolean)cache.containsKey(endpoint + '/' + func.func)).toString());
+		if (!cache.containsKey(endpoint + '/' + func.func))
+			return false;
+		System.out.println(func.toString() + " in cache? " + ((Boolean)cache.get(endpoint + '/' + func.func).containsKey(new ArrayWrapper(func.args))).toString());
+		return cache.containsKey(endpoint + '/' + func.func) && cache.get(endpoint + '/' + func.func).containsKey(new ArrayWrapper(func.args)); 
+	}
+	private Object get(String endpoint, FunctionCall func) {
+		return cache.get(endpoint + '/' + func.func).get(new ArrayWrapper(func.args));
+	}
 	public Object __rpccall__(FunctionCall func) {
-		return __rpccall__(this.endpoint, func);
+		return __rpccall__(this.endpoint, func, true);
 	}
 	public Object __rpccall__(String endpoint, FunctionCall func) {
+		return __rpccall__(endpoint, func, true);
+	}
+	public Object __rpccall__nocache(FunctionCall func) {
+		return __rpccall__(this.endpoint, func, false);
+	}
+	public Object __rpccall__nocache(String endpoint, FunctionCall func) {
+		return __rpccall__(endpoint, func, false);
+	}
+	public synchronized Object __rpccall__(String endpoint, FunctionCall func, boolean usecache) {
+		if (usecache && has(endpoint, func))
+			return get(endpoint, func);
 		func.args = __marshall_args__(func.args);
 		HttpPost post = new HttpPost(endpoint);
 		StringEntity req = null;
@@ -151,15 +182,25 @@ public abstract class RpcClient {
 			e.printStackTrace();
 			return null;
 		}
+		if (usecache) set(endpoint, func, ret);
 	}
 	public Object call(String func, Object... args) {
 		return __rpccall__(this.endpoint, new FunctionCall(func, args));
+	}
+	public Object call_nocache(String func, Object... args) {
+		return __rpccall__(this.endpoint, new FunctionCall(func, args), false);
 	}
 	public Object get(String attr) {
 		return __rpccall__(this.base_endpoint, 
 				new FunctionCall("globals.getattr", 
 						String.format("hash:%d",this.interface_.hash), 
 						attr));
+	}
+	public Object get_nocache(String attr) {
+		return __rpccall__(this.base_endpoint, 
+				new FunctionCall("globals.getattr", 
+						String.format("hash:%d",this.interface_.hash), 
+						attr), false);
 	}
 	public void set(String attr, Object value) {
 		__rpccall__(this.base_endpoint, new FunctionCall("globals.setattr", attr, value));
